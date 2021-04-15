4cad01a730f020ca66ace57bb63767407e09dff5
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-14 21:58:38 -0500

    Added routes for '/*' and post '/api/notes'

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -13,10 +13,18 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Define routes
-console.log("Path: "+__dirname);
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './notes.html')));
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '..\\db\\db.json')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    
+    console.log("newNote: "+newNote.title);
+});
+
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
