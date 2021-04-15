ce0f7b4b7554135a158d18afb081f9e90f244398
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-14 20:42:21 -0500

    Defined routes to '/', 'notes', '/api/notes'

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 // Set up the Express App
 const app = express();
@@ -9,6 +10,13 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+// Define routes
+console.log("Path: "+__dirname);
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '..\\db\\db.json')));
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
